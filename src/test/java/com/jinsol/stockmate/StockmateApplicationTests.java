912f5358077811eb4f1640cee3909ab8df8b4851
jskim <jskim@insideinfo.co.kr>
2026-07-14 17:16:02 +0900

    chore: initialize Spring Boot Project

---
+++ src/test/java/com/jinsol/stockmate/StockmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinsol.stockmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
