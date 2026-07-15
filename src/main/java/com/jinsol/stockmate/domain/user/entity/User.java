969e16b3dbdd08f075493e9ef5274ab446ec0965
jskim <jskim@insideinfo.co.kr>
2026-07-15 17:27:34 +0900

    feat: User 엔티티 및 Role enum 추가

---
+++ src/main/java/com/jinsol/stockmate/domain/user/entity/User.java
@@ -0,0 +1,41 @@
+package com.jinsol.stockmate.domain.user.entity;
+
+import com.jinsol.stockmate.domain.user.enums.Role;
+import com.jinsol.stockmate.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access =  AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String email, String password, String name, Role role) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/jinsol/stockmate/domain/user/enums/Role.java
@@ -0,0 +1,6 @@
+package com.jinsol.stockmate.domain.user.enums;
+
+public enum Role {
+    ADMIN,      //관리자
+    USER        //일반 사용자
+}
