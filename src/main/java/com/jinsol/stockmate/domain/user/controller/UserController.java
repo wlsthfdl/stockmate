a55dd5120d4f9865c4dd9d6475f7a04fefb901fc
wlsthfdl <98313929+wlsthfdl@users.noreply.github.com>
2026-08-07 09:34:46 +0900

    feat: User 회원가입 controller 구현

---
+++ src/main/java/com/jinsol/stockmate/domain/user/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.jinsol.stockmate.domain.user.controller;
+
+import com.jinsol.stockmate.domain.user.dto.UserResponse;
+import com.jinsol.stockmate.domain.user.dto.UserSignupRequest;
+import com.jinsol.stockmate.domain.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    //회원가입
+    @PostMapping("/signup")
+    public ResponseEntity<UserResponse> signup(@Valid @RequestBody UserSignupRequest request){
+        UserResponse response = userService.signup(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
