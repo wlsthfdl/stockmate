5a3a9db95b68a81bd86668b1e8394b73ed3beb9d
wlsthfdl <98313929+wlsthfdl@users.noreply.github.com>
2026-08-07 13:40:09 +0900

    feat: JWT 발급/검증 유틸리티(JwtProvider)추가

--- src/main/java/com/jinsol/stockmate/global/config/SecurityConfig.java
+++ src/main/java/com/jinsol/stockmate/global/config/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig {
 
+        //특정 http요청에 대해 웹 기반 보안 구성.
+        //csrf: 공격자가 인증된 브라우저에 저장된 쿠키 세션정보를 활용하여 웹 서버에 사용자가 의도하지 않은 요청을 전달하는 것
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
             http
---
+++ src/main/java/com/jinsol/stockmate/global/jwt/JwtProvider.java
@@ -0,0 +1,61 @@
+package com.jinsol.stockmate.global.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+    private final SecretKey secretKey;
+    private final long expiration;
+
+    public JwtProvider(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.expiration}") long expiration
+    ){
+        //문자열로된 비밀키를 jwt라이브러리가 요구하는 암호화 키 객체로 변환하는 과정
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+        this.expiration = expiration;
+    }
+
+    //토큰 생성
+    public String createToeken(String email){
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime()+expiration);
+
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(now)      //발급 시각
+                .expiration(expiryDate)     //만료 시각
+                .signWith(secretKey)        //비밀키 서명
+                .compact();
+    }
+
+    //토큰에서 이메일 추출
+    public String getEmail(String token){
+        Claims claims = Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return claims.getSubject();
+    }
+
+    //토큰 유효성 검증
+    public boolean validateToken(String token){
+       try{
+           Jwts.parser()
+                   .verifyWith(secretKey)
+                   .build()
+                   .parseSignedClaims(token);
+           return true;
+       }catch (Exception e){
+           return false;
+       }
+    }
+}
